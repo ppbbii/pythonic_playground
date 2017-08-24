9a85fde71aa55954fcebfc68723192e5000229a9
Przemyslaw Iwanczak <ppbbii@users.noreply.github.com>
2017-08-24 10:02:27 +0200

    Add test for find play

--- find_play/findfind.py
+++ find_play/findfind.py
@@ -2,13 +2,19 @@ import argparse
 import re
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dir", nargs='?', default="/usr/local/", help="optionally provide location where the find should take place")
-parser.add_argument("-n", "--name", help="find name by regex", required=False)
-parser.add_argument("-p", "--print", help="print matching locations", required=False)
-args = parser.parse_args()
 
 
-for item in os.listdir(args.dir):
-    if re.search(args.name, item):
-        print("{}/{}".format(args.dir,item))
+def run_job():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", nargs='?', default="/usr/local/",
+                        help="optionally provide location where the find should take place")
+    parser.add_argument("-n", "--name", help="find name by regex", required=False)
+    parser.add_argument("-p", "--print", help="print matching locations", required=False)
+    args = parser.parse_args()
+
+    for item in os.listdir(args.dir):
+        if re.search(args.name, item):
+            print("{}/{}".format(args.dir,item))
+
+if __name__ == '__main__':
+    run_job()
---
+++ find_play/test_find.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+from contextlib import contextmanager
+
+import sys
+
+from find_play.findfind import run_job
+
+class TestFind(unittest.TestCase):
+    def test_expected_output(self):
+        CONFIG_PATH = os.path.dirname(os.path.abspath(__file__))
+        sys.argv = ['', CONFIG_PATH, '-n', '\w+']
+        ex_output = '''{0}/test_find.py\n{1}/findfind.py'''\
+            .format(CONFIG_PATH, CONFIG_PATH)
+
+        with capture_output() as (out, err):
+
+            run_job()
+
+        assert out is not None
+        rl_output = out.getvalue().strip()
+        assert rl_output == ex_output
+
+@contextmanager
+def capture_output():
+    from io import StringIO
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
